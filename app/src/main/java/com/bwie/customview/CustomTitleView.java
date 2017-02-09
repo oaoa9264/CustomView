c301786c8778b8f19b1fcdaea8280f61f74fcc49
oaoa9264 <212319919@qq.com>
2017-02-09 10:02:49 +0800

    加一行注释

--- app/src/main/java/com/bwie/customview/CustomTitleView.java
+++ app/src/main/java/com/bwie/customview/CustomTitleView.java
@@ -113,6 +113,10 @@ public class CustomTitleView extends View {
 
     }
 
+    /**
+     * 生成随机数
+     * @return
+     */
     private String randomText()
     {
         Random random = new Random();
